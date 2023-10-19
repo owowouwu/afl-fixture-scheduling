4324775b76f360113741a9eea1c9e82bb6cc848f
leilahtaouk <ltaouk@student.unimelb.edu.au>
2023-10-19 13:41:20 +1100

    Cleaned repo

--- Formulation_Genesis.py
+++ Formulation_Genesis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-import gurobipy as gp
-from gurobipy import GRB
+import gurobipy as GRB 
+from gurobipy import *
 import pickle
 
 # Parameters
@@ -12,7 +12,6 @@ teams = ["Adelaide Crows", "Brisbane Lions", "Carlton Blues", "Collingwood Magpi
      "St Kilda Saints", "Sydney Swans", "West Coast Eagles", "Western Bulldogs"]
 
 team_numbers = {team: number for number, team in enumerate(teams, start=0)}
-print(team_numbers['Carlton Blues'])
 
 locations = ['Victoria','NSW','SA','WA','QLD']
 location_numbers = {location: number for number, location in enumerate(locations, start=0)}
@@ -20,13 +19,21 @@ location_numbers = {location: number for number, location in enumerate(locations
 
 home_locations = ['SA','QLD','Victoria','Victoria','Victoria','WA','Victoria','QLD','NSW','Victoria',
                           'Victoria','Victoria','SA','Victoria','Victoria','NSW','WA','Victoria']
+#2022 Stats
 
+ranking = [14,9,6,4,15,5,1,12,16,13,2,18,11,7,10,3,17,8] # 2022 post-finals rankings
 
-ranking = [10,2,5,1,11,14,12,15,7,16,4,17,3,13,6,8,18,9] # 2023 pre-finals rankings
+wins = [8,15,12,16,7,15,18,10,6,8,16,2,10,13,11,16,2,12] # 2022 pre-finals wins
 
-wins = [11,17,13.5,18,11,10,10.5,9,13,7,16,3,17,10.5,13,12.5,3,12]
+team_fans = [63,43,89,100,86,56,72,21,30,81,66,50,59,101,60,55,103,51] # 2022 number of members, in 000's
 
-team_fans = [60,45,93,102,79,61,81,20,33,72,68,50,60,100,58,63,100,55] # 2023 number of members, in 000's
+#2023 Stats 
+
+# ranking = [10,2,5,1,11,14,12,15,7,16,4,17,3,13,6,8,18,9] # 2023 pre-finals rankings
+
+# wins = [11,17,13.5,18,11,10,10.5,9,13,7,16,3,17,10.5,13,12.5,3,12]
+
+# team_fans = [60,45,93,102,79,61,81,20,33,72,68,50,60,100,58,63,100,55] # 2023 number of members, in 000's
 
 stadiums = ['MCG','Marvel','GMHBA','Adelaide Oval','Optus','Gabba','HBS','SCG','Giants']
 stadium_numbers = {stadium: number for number, stadium in enumerate(stadiums, start=0)}
@@ -146,7 +153,7 @@ def generate_initial_fixture():
 
 
     # Create a new model
-    model = gp.Model("fixture_scheduling")
+    model = Model("fixture_scheduling")
     model.setParam('Timelimit', 7200)
     model.setParam("MIPGap", 0.01)
     
@@ -156,86 +163,86 @@ def generate_initial_fixture():
 
 
     fixture = model.addVars(index,vtype=GRB.BINARY)
-    game_on = model.addVars(timeslots_index,vtype=GRB.BINARY)
+    #game_on = model.addVars(timeslots_index,vtype=GRB.BINARY)
     equality = model.addVars([(i) for i in Ts])
         
     
     for i in Ts: # Each team plays once a week
         for r in rounds:
-            model.addConstr(gp.quicksum(fixture[i, j, s, t, r] + fixture[j, i, s, t, r] for j in Ts for s in Ss for t in timeslots) == 1, "MatchesEachRound")
+            model.addConstr(quicksum(fixture[i, j, s, t, r] + fixture[j, i, s, t, r] for j in Ts for s in Ss for t in timeslots) == 1, "MatchesEachRound")
             
     
     for i in Ts: # 11 Home Games 
-        model.addConstr(gp.quicksum(fixture[i, j, stadium_numbers[s], t, r] for j in Ts for s in home_stadiums[i] 
+        model.addConstr(quicksum(fixture[i, j, stadium_numbers[s], t, r] for j in Ts for s in home_stadiums[i] 
                                  for t in timeslots for r in rounds) == 11, f"HomeGames_{i}")
     
     
     for i in Ts:
-        model.addConstr(gp.quicksum(fixture[i, i, s, t, r] for s in Ss for t in timeslots for r in rounds) <= 0, f"DoNotPlaySelf")
+        model.addConstr(quicksum(fixture[i, i, s, t, r] for s in Ss for t in timeslots for r in rounds) <= 0, f"DoNotPlaySelf")
         for j in Ts:
             if i != j:
-                model.addConstr(gp.quicksum(fixture[i, j, s, t, r] for s in Ss for t in timeslots for r in rounds) <= 1, f"PlayMoreThanTwice_{i}_{j}")
-                model.addConstr(gp.quicksum(fixture[i, j, s, t, r] + fixture[j, i, s, t, r] for s in Ss for t in timeslots for r in rounds) >= 1, f"PlayAtLeastOnce_{i}_{j}")
+                model.addConstr(quicksum(fixture[i, j, s, t, r] for s in Ss for t in timeslots for r in rounds) <= 1, f"PlayMoreThanTwice_{i}_{j}")
+                model.addConstr(quicksum(fixture[i, j, s, t, r] + fixture[j, i, s, t, r] for s in Ss for t in timeslots for r in rounds) >= 1, f"PlayAtLeastOnce_{i}_{j}")
                 
                 
     for i in Ts:
         for r in rounds[:-1]:
-            model.addConstr(1 >= gp.quicksum(fixture[i, j, s, t, r]+fixture[j, i, s, t, r] for j in Ts for s in Ss for t in [5,6]) + 
-                            gp.quicksum(fixture[i, j, s, t, r+1]+fixture[j, i, s, t, r+1] for j in Ts for s in Ss for t in [0]),
+            model.addConstr(1 >= quicksum(fixture[i, j, s, t, r]+fixture[j, i, s, t, r] for j in Ts for s in Ss for t in [5,6]) + 
+                            quicksum(fixture[i, j, s, t, r+1]+fixture[j, i, s, t, r+1] for j in Ts for s in Ss for t in [0]),
                            f"AtLeastAFiveDayBreak_{i}")
             
     
     # Three games in a row outside home location
     for i in Ts:
         for r in rounds[:-2]:
-            model.addConstr(gp.quicksum(fixture[j, i, stadium_numbers[s], t, r_]+fixture[i,j, stadium_numbers[s], t, r_] for j in Ts for s in home_location_stadiums[i] 
+            model.addConstr(quicksum(fixture[j, i, stadium_numbers[s], t, r_]+fixture[i,j, stadium_numbers[s], t, r_] for j in Ts for s in home_location_stadiums[i] 
                                  for t in timeslots for r_ in range(r,r+3)) >= 1, f"ThreeGamesInRowOutside_{i}_{r}")
        
     # Four away games in a row
     for i in Ts:
         for r in rounds[:-3]:
-            model.addConstr(gp.quicksum(fixture[i, j, s, t, r_] for j in Ts for s in Ss for t in timeslots
+            model.addConstr(quicksum(fixture[i, j, s, t, r_] for j in Ts for s in Ss for t in timeslots
                                      for r_ in range(r,r+4)) >= 1, f"FourAwayGamesInRow_{i}_{r}")
     
     
     # Constraint 7: 2+ games in one day in the same stadium
     for r in rounds:
         for s in Ss:
-            model.addConstr(gp.quicksum(fixture[i, j, s, t, r] for i in Ts for j in Ts for t in [5, 6]) <= 1, f"SundayGamesInStadium_{r}_{s}")
+            model.addConstr(quicksum(fixture[i, j, s, t, r] for i in Ts for j in Ts for t in [5, 6]) <= 1, f"SundayGamesInStadium_{r}_{s}")
             
-            model.addConstr(gp.quicksum(fixture[i, j, s, t, r] for i in Ts for j in Ts for t in [2, 3, 4]) <= 1, f"SaturdayGamesInStadium_{r}_{s}")
+            model.addConstr(quicksum(fixture[i, j, s, t, r] for i in Ts for j in Ts for t in [2, 3, 4]) <= 1, f"SaturdayGamesInStadium_{r}_{s}")
             
             for t in [0,1]:
-                model.addConstr(gp.quicksum(fixture[i, j, s, t, r] for i in Ts for j in Ts) <= 1, f"OneGameAtATimeInStadium_{r}_{s}")
+                model.addConstr(quicksum(fixture[i, j, s, t, r] for i in Ts for j in Ts) <= 1, f"OneGameAtATimeInStadium_{r}_{s}")
     
     
     # Constraint: No more than two games in any timeslot, and only one on Thursday and Friday night
     for r in rounds:
         
-        model.addConstr(gp.quicksum(fixture[i, j, s, t, r] for i in Ts for j in Ts for s in Ss for t in [5,6]) >= 2, f"AtLeastTwoSundayGames")
+        model.addConstr(quicksum(fixture[i, j, s, t, r] for i in Ts for j in Ts for s in Ss for t in [5,6]) >= 2, f"AtLeastTwoSundayGames")
         
         for t in [0,1]:
-            model.addConstr(gp.quicksum(fixture[i, j, s, t, r] for i in Ts for j in Ts for s in Ss) <= 1, f"OneThursday&FridayNightGame_{r}")
+            model.addConstr(quicksum(fixture[i, j, s, t, r] for i in Ts for j in Ts for s in Ss) <= 1, f"OneThursday&FridayNightGame_{r}")
             
         for t in [2,3,4,5,6]:
-            model.addConstr(gp.quicksum(fixture[i, j, s, t, r] for i in Ts for j in Ts for s in Ss) <= 2, f"NoMoreThanTwoSimultaneousGames_{r}")
+            model.addConstr(quicksum(fixture[i, j, s, t, r] for i in Ts for j in Ts for s in Ss) <= 2, f"NoMoreThanTwoSimultaneousGames_{r}")
         
-        for t in timeslots:
-            model.addConstr(gp.quicksum(fixture[i, j, s, t, r] for i in Ts for j in Ts for s in Ss) >= game_on[t,r], f"IncentiviseAtLeastOneGameInEachTimeslot_{r}_{t}")
+        #for t in timeslots:
+         #   model.addConstr(quicksum(fixture[i, j, s, t, r] for i in Ts for j in Ts for s in Ss) >= game_on[t,r], f"IncentiviseAtLeastOneGameInEachTimeslot_{r}_{t}")
             
     
-           
+    # These find the absolute value for how many wins away from the AFL average each team is (the average numbers of wins in a 22 game season is 11).    
     for i in Ts:
-        model.addConstr(equality[i] >= gp.quicksum(probability_win(i, j, s)*fixture[i,j,s,t,r] + (1-probability_win(j,i,s))*fixture[j,i,s,t,r] 
+        model.addConstr(equality[i] >= quicksum(probability_win(i, j, s)*fixture[i,j,s,t,r] + (1-probability_win(j,i,s))*fixture[j,i,s,t,r] 
                                                    for j in Ts for s in Ss for t in timeslots for r in rounds)-11)
                 
-        model.addConstr(equality[i] >= 11-gp.quicksum(probability_win(i, j, s)*fixture[i,j,s,t,r] + (1-probability_win(j,i,s))*fixture[j,i,s,t,r] 
+        model.addConstr(equality[i] >= 11-quicksum(probability_win(i, j, s)*fixture[i,j,s,t,r] + (1-probability_win(j,i,s))*fixture[j,i,s,t,r] 
                                                       for j in Ts for s in Ss for t in timeslots for r in rounds))
 
-
-    model.setObjective(gp.quicksum(attractiveness(i,j,s,t,r)*fixture[i,j,s,t,r] for i in Ts for j in Ts for s in Ss for t in timeslots for r in rounds) 
-                       - 10*gp.quicksum(equality[i] for i in Ts), GRB.MAXIMIZE)
-    
+    # We remove the requirement of at least one game in each timeslot, it leads to errors
+    model.setObjective(quicksum(attractiveness(i,j,s,t,r)*fixture[i,j,s,t,r] for i in Ts for j in Ts for s in Ss for t in timeslots for r in rounds) 
+                       - 100*quicksum(equality[i] for i in Ts), GRB.MAXIMIZE)
+    #+ 250*quicksum(game_on[t,r] for t in timeslots for r in rounds) 
     
     model.optimize()
     
@@ -320,8 +327,8 @@ def feasibility(fixture):
             violated += max(0,1-sum(fixture[i][j][s][t][r] for i in Ts for j in Ts for s in Ss)) # At least one game each timeslot
             violated += max(0,sum(fixture[i][j][s][t][r] for i in Ts for j in Ts for s in Ss)-2)
         
-        for t in [0,1]:
-            violated += max(0,sum(fixture[i][j][s][t][r] for i in Ts for j in Ts for s in Ss)-1) # One game
+        #for t in [0,1]:
+         #   violated += max(0,sum(fixture[i][j][s][t][r] for i in Ts for j in Ts for s in Ss)-1) # One game
             
             
     return violated, critical
@@ -346,13 +353,29 @@ def fixture_attractiveness(fixture,max_value,violated_factor,critical_factor,equ
     
     return total_score - violated_factor*violated - critical_factor*critical - equality
 
+def main():
   
-MILP_fixture, MILP_value = generate_initial_fixture()
-print(MILP_value)  
+    MILP_fixture, MILP_value = generate_initial_fixture()
+    print(MILP_value)  
+
+    max_value,violated_factor,critical_factor,equality_factor = 2*(10**4),2*10**4,10**6,10**3
+    value = fixture_attractiveness(MILP_fixture,max_value,violated_factor,critical_factor,equality_factor)
+
+    print(value)
+    path = 'initialPopulation'
+    with open(f'{path}\MILP_Fixture_2022.pkl', 'wb') as file:
+        pickle.dump(MILP_fixture, file)
+
+if __name__ == '__main__':
+    main()
+
+  
+# MILP_fixture, MILP_value = generate_initial_fixture()
+# print(MILP_value)
 
 max_value,violated_factor,critical_factor,equality_factor = 2*(10**4),2*10**4,10**6,10**3
+MILP_fixture, MILP_value = generate_initial_fixture()
 value = fixture_attractiveness(MILP_fixture,max_value,violated_factor,critical_factor,equality_factor)
-
 print(value)
 print(MILP_fixture)
-np.save('MILP_Fixture_10000.npy', MILP_fixture)
+np.save('MILP_Fixture_100_2022.npy', MILP_fixture)
